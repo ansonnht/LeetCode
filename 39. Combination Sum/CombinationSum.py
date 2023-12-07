c050e9ffa19b4229a681999f5f8e523cc40cfbfe
Anson Ng <anson9823@gmail.com>
2023-12-07 21:01:05 +0800

    LeetCode 39 Update

--- 39. Combination Sum/CombinationSum.py
+++ 39. Combination Sum/CombinationSum.py
@@ -1,9 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        def dfs(i, subset, total):
-            if total == target:
+        subset = []
+        def dfs(i):
+            if sum(subset) == target:
                 res.append(subset.copy())
+                return
+            if i >= len(candidates) or sum(subset) > target:
+                return
+            subset.append(candidates[i])
+            dfs(i)
+            subset.pop()
+            dfs(i+1)
+        dfs(0)
+        return res
+
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
